ecae156106250f1f0707b603bf6c5d78f9a34835
felipesqra <felipe.siqueira@ccc.ufcg.edu.br>
2024-05-30 21:55:03 -0300

    adaptando para utilizar 100% da tela

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ChatBot from 'react-simple-chatbot';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const steps = [
+    {
+      id: '1',
+      message: 'Olá, qual é o seu nome?',
+      trigger: '2',
+    },
+    {
+  id: '2',
+  user: true,
+  trigger: '3',
+    },
+    {
+  id: '3',
+  message: 'Olá {previousValue}, como posso ajudar?',
+  end: true,
+    }
+  ];
+
+  const style = {
+    width: '100%',
+    height: '100vh',
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    border: 'none'
+  };
+
+  return <ChatBot steps={steps} style={style}/>;
 }
 
 export default App;
